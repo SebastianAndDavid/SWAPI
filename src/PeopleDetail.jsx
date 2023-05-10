e77e616945537243e2b0739e7659c26a2de0b636
David <Rigby335@hotmail.com>
2023-05-10 10:05:54 -0700

    ft. PeopleDetail planning

--- src/PeopleDetail.jsx
+++ src/PeopleDetail.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function PeopleDetail() {
-  return <div>PeopleDetail</div>;
+  return (
+    <div>
+      {/* one div will render people details w/o any endpoint */}
+
+      {/* other div will render multiple <components/> that catch props to render the endpoint info */}
+
+      {/* <component vehicle={vehicle} homeworld={homeworld}/> */}
+    </div>
+  );
 }
 
 export default PeopleDetail;
